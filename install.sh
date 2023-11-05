622dbc4f00fe01d2e1de23e5dada139994c274dd
dsesclei <dave@sescleifer.com>
2023-11-05 18:00:31 -0500

    .

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 trap 'echo "Error at line $LINENO"' ERR
 
 sudo apt update && sudo apt -y install zsh
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $(whoami)
 curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 curl -sS https://starship.rs/install.sh | sh
