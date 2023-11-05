0a33ce3348c6e293c345edd28e2f5d56ab6c7b1a
dsesclei <dave@sescleifer.com>
2023-11-05 17:26:56 -0500

    .

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+trap 'echo "Error at line $LINENO"' ERR
+
 sudo apt update && sudo apt -y install zsh
 chsh -s $(which zsh)
 curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
@@ -5,5 +10,3 @@ curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | 
 curl -sS https://starship.rs/install.sh | sh
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 cp .zshrc $HOME
-
-
