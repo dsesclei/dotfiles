fe3af08837bac3a404714ada945dea6046221de9
dsesclei <dave@sescleifer.com>
2023-11-05 20:43:27 -0500

    .

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ sudo chsh -s $(which zsh) $(whoami)
 curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 cp .zshrc $HOME
